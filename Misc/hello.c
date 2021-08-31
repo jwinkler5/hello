e9f2322f779f233f2d701601bfbc4c97ef0ba3b6
Oleg Kojenets <okojenets2@huskers.unl.edu>
2021-08-31 01:47:08 +0000

    Partners Change

--- Misc/hello.c
+++ Misc/hello.c
@@ -10,7 +10,9 @@
   #include <stdio.h>
 
   int main(int argc, char **argv) {
-printf("jackson winkler/n")
+ printf("jackson winkler/n")
  printf("Hello World!/n");
+ printf("Partners name: Oleg Kojenets\n");
+ printf("Partners major: Computer Science\n");
  return 0;
  }
